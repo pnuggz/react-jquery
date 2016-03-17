83219d446d993a5445f3c74f22b7e94df7ee8edb
Jay Phelps <hello@jayphelps.com>
2016-03-16 22:25:41 -0700

    add some inline explaination to examples

--- examples/lazy-dom/jquery/app.js
+++ examples/lazy-dom/jquery/app.js
@@ -1,3 +1,21 @@
+/**
+ * One of the many issues with virtual DOM instead of real DOM
+ * is that it makes it awkward (or sometimes even impossible)
+ * to integrate with libraries that need access to the real DOM.
+ * 
+ * lazy-dom elements can be consumed exactly how you would
+ * would consume real DOM elements! The trick is a bit complicated
+ * but the idea is that lazy-dom elements are actually more like
+ * "element placeholders" similar to virtual DOM elements
+ * except they can be consumed by code expecting a real DOM
+ * element and will lazily create the real DOM element
+ * and proxy into it when neccesary.
+ * 
+ * Things get much more complicated when dealing with diffs
+ * so I encourage you to read the ARCHITECTURE.md doc if
+ * you really would like to know how it works.
+ */
+
 $.fn.counter = function counter() {
   let count = 0;
 
@@ -16,10 +34,10 @@ $.fn.counter = function counter() {
 };
 
 const Application = () => {
-  let counter = <div id="suchwow" />;
+  let counter = <div id="counter" />;
 
-  // lazy-dom JSX elements can be treated as if they
-  // they were actual DOM nodes!
+  // lazy-dom JSX elements can be treated as
+  // if they they were actual DOM nodes!
   $(counter).counter();
 
   return (
--- examples/lazy-dom/simplest/app.js
+++ examples/lazy-dom/simplest/app.js
@@ -1,7 +1,19 @@
-// With lazy-dom, you can treat JSX elements
-// as if they were real DOM nodes.
+/**
+ * This example is fairly worthless in practice
+ * but it eases you into understanding that with
+ * lazy-dom, you treat your JSX as if it were
+ * real DOM. Quite literally re-render your app
+ * every time as if you were creating the DOM
+ * for the first time.
+ * 
+ * You'll learn about how to then apply the diff
+ * of your new DOM to the old DOM in the more
+ * complex examples like Todos.
+ */
+
 var container = <div id="container" />;
 container.appendChild(<h1>hello, world.</h1>);
 
 // Now we can just put everything in the <body>
+// again, just treat them like real DOM.
 document.body.appendChild(container);
